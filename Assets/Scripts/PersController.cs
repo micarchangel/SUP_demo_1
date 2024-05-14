6d16ed7e76f1823c79bde8cacbabe7c9bce19e85
NessRnD <nessrnd@gmail.com>
2024-05-14 23:04:35 +0300

    mayCommit
    Добавили карту и управление

---
+++ Assets/Scripts/PersController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PersController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float moveSpeed = 1f;
+    Vector3 direction;
+    CharacterController controller;
+
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        direction = new Vector3(-x*speed, 0, -(moveSpeed + z));
+
+        controller.Move(direction * Time.deltaTime);
+    }
+}
