cd8cc671a2b3b6c1554a5aa70c3ea878b2d3d186
micarchangel <obi3656625@gmail.com>
2024-07-11 23:46:07 +0300

    Сделал загрузку и изменение глобальной громкости
    не могу разобраться с выпадающим списком

--- Assets/Scripts/GameSettings.cs
+++ Assets/Scripts/GameSettings.cs
@@ -2,16 +2,34 @@
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using Unity.VisualScripting;
 
 public class GameSettings : MonoBehaviour
 {
-    [SerializeField] private Slider slider;
+    
+    [SerializeField] private Dropdown qualityValue;
     [SerializeField] private float audioVolume;
+    [SerializeField] private Slider slider;
     [SerializeField] private AudioMixer am;
+    [SerializeField] private int VolCoeff;
+
+    void Start()
+    {
+        am.SetFloat("masterVolume", Mathf.Log10(audioVolume) * VolCoeff);
+
+        if (slider != null)
+        {
+            // Привязка функции к изменению значения слайдера
+            slider.onValueChanged.AddListener(AudioVolume);
+        }
+
+        //qualityValue.onValueChanged.AddListener(Quality);
+    }
 
     public void FullScreenToggle()
     {
         Screen.fullScreen = !Screen.fullScreen;
+        Debug.Log(Screen.fullScreen);
     }
 
     public void SaveSliderValue()
@@ -21,12 +39,18 @@ public class GameSettings : MonoBehaviour
         Debug.Log("saved " + audioVolume);
     }
 
+    //public void SaveDropdownValue()
+    //{
+    //    PlayerPrefs.SetInt("DropdownValue", qualityValue);
+    //}
+
     //public void OnSceneChange(Scene scene, LoadSceneMode mode)
     //{
     //    // Сохраняем значение слайдера перед переходом к другой сцене
     //    SaveSliderValue();
     //}
 
+    //Управление качеством
     public void Quality(int q)
     {
         QualitySettings.SetQualityLevel(q);
@@ -42,5 +66,11 @@ public class GameSettings : MonoBehaviour
     void OnDisable()
     {
         SaveSliderValue();
+    }
+
+    // Управление громкостью слайдером
+    void AudioVolume (float sliderValue) {
+        am.SetFloat("masterVolume", Mathf.Log10(sliderValue) * VolCoeff);
+        audioVolume = sliderValue;
     }
 }
