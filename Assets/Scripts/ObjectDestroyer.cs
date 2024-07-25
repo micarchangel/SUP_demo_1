45eff4b39503a362f543b6ed2c7101b85d2c394a
sergey <rates-13@inbox.ru>
2024-07-25 06:30:35 +0300

    WaterObject
    Добавил все ранее не совмещенное, Финиш лайн, точки генерации, плавающие боксы, в процессе уничтожение объектов.

---
+++ Assets/Scripts/ObjectDestroyer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDestroyer : MonoBehaviour
+{
+    public Transform player;
+    public float destroyDistanceBehind = 20.0f;
+
+    void Update()
+    {
+        if (player != null && transform.position.z < player.position.z - destroyDistanceBehind)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Obstacles/FloatingBox.cs
+++ Assets/Scripts/Obstacles/FloatingBox.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FloatingBox : MonoBehaviour
 {
-    public float speed = 1.0f; // Скорость движения ящика
+    public float speed = 0.3f; // Скорость движения ящика
     public float changeDirectionTime = 2.0f; // Время до смены направления
 
     private Vector3 direction; // Текущее направление движения
