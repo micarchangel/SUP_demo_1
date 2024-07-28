aea705ff4bae4c02ff1da205d1bdee8bb9313eb6
micarchangel <obi3656625@gmail.com>
2024-07-28 22:27:32 +0300

    Сделал счетчик
    Не работает с буем - судя по всему дело в корутине - надо подумать.

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Obstacle : MonoBehaviour
 {
     [SerializeField] float _damage;
+    [SerializeField] int _score;
 
     public float Damage => _damage;
+    public int Score => _score;
 }
