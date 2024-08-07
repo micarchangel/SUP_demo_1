26fbd443248e8b78ec7624115f7a51f7e331d4a7
micarchangel <obi3656625@gmail.com>
2024-08-07 23:11:43 +0300

    07.08 (2)
    Исправлен звук взрыва - привязан к аудиомиксеру. Мелкие исправления интерфейса

--- Assets/Scripts/Obstacles/ExplodeAndRestartGame.cs
+++ Assets/Scripts/Obstacles/ExplodeAndRestartGame.cs
@@ -1,17 +1,28 @@
+using System.Collections;
 using UnityEngine;
 
 public class ExplodeAndRestartGame : MonoBehaviour
 {
     [SerializeField] private GameObject explosionEffect;
-    [SerializeField] private AudioClip explosionSound;
+    [SerializeField] private AudioSource explosionSource;
+    [SerializeField] private GameObject seaMineMesh;
+    [SerializeField] private float delay;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log("Взрыв");
-            AudioSource.PlayClipAtPoint(explosionSound, transform.position);
+            explosionSource.Play();
             Instantiate(explosionEffect, transform.position, transform.rotation);
-            Destroy(gameObject);
+            seaMineMesh.SetActive(false);
+            StartCoroutine(delayCouroutine(delay));
         }
+    }
+
+    private IEnumerator delayCouroutine(float delay)
+    { 
+        yield return new WaitForSeconds(delay);
+        Destroy(gameObject);
     }
 }
