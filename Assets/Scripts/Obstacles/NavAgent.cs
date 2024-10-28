70947374cfe0f7b14da730d862a0923a216e4885
micarchangel <obi3656625@gmail.com>
2024-10-28 19:14:51 +0300

    28.10.2024
    Созданы NavMeshAgent-ы на всех лодках
    На некоторых объектах сознаны NavMesh Obstacle

--- Assets/Scripts/Obstacles/NavAgent.cs
+++ Assets/Scripts/Obstacles/NavAgent.cs
@@ -6,13 +6,15 @@ using UnityEngine.AI;
 public class NavAgent : MonoBehaviour
 {
     //[SerializeField] private Vector3 XYZ;
-    [SerializeField] private Transform wayPoint;
-
+    private Transform wayPoint;
     private NavMeshAgent _agent;
 
     void Start()
     {
+        wayPoint = GameObject.FindGameObjectWithTag("WayPoint").transform;
         _agent = GetComponent<NavMeshAgent>();
-        _agent.SetDestination(wayPoint.position);
+        if(wayPoint != null)
+            _agent.SetDestination(wayPoint.position);
+        Debug.Log($"{gameObject.name} : Точка назначения = {wayPoint.position}");
     }
 }
