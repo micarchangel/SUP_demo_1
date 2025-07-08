111b5dad84307b8c403b7577b5808b80ce906e5c
Roman Baybara <130989544+NessRnD@users.noreply.github.com>
2025-07-08 15:23:27 +0300

    fix
    починил сцену после мержа

--- Assets/Scripts/Bots/BotAnimation.cs
+++ Assets/Scripts/Bots/BotAnimation.cs
@@ -1,61 +1,166 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class BotAnimation : MonoBehaviour
 {
-    [SerializeField] private GameObject playerModel;
-    [SerializeField] private GameObject veslo;
-    private Animator animator;
-    private ChangeParentVeslo animatorVeslo;
-    private Vector3 lastPosition;
-    private Quaternion lastRotation;
-
-    private float h;
-    private float v;
-
-    void Start()
+    [System.Serializable]
+    public class CharacterSetup
     {
-        lastRotation = GetComponentInParent<Transform>().rotation;
-        lastPosition = GetComponentInParent<Transform>().position;
-        animator = playerModel.GetComponent<Animator>();
-        animatorVeslo = veslo.GetComponent<ChangeParentVeslo>();
+        public GameObject model;
+        public GameObject veslo;
     }
 
-    void Update()
+    [SerializeField]
+    private CharacterSetup[] characterSetups;
+
+    private GameObject activeModel;
+    private GameObject activeVeslo;
+    private Animator animator;
+    private ChangeParentVeslo vesloController;
+    private Vector3 lastPosition;
+    private Quaternion lastRotation;
+    private float horizontalInput;
+    private float verticalInput;
+    private bool isMoving;
+
+    private void Start()
     {
-        h = Mathf.Sign(lastRotation.y - GetComponentInParent<Transform>().rotation.y);
-        v = Mathf.Sign(lastPosition.z - GetComponentInParent<Transform>().position.z);
+        InitializeRandomCharacter();
+        lastRotation = transform.parent.rotation;
+        lastPosition = transform.parent.position;
+    }
 
-        Debug.Log($"{h} {v}");
-
-        animator.SetFloat("verticalMove", v);
-        animator.SetFloat("horizontalMove", v);
-
-        if (h != 0 || v != 0)
+    private void Update()
+    {
+        if (activeModel == null || !activeModel.activeSelf)
         {
-            //animator.SetBool("SwimForward", true);
-            //animator.SetBool("SwimBackward", false);
-            animator.SetBool("isMove", true);
-
-        }
-        else
-        {
-            animator.SetBool("isMove", false);
-            //animator.SetBool("SwimForward", false);
-            //animator.SetBool("SwimBackward", true);
-
+            return;
         }
 
+        CalculateMovementInput();
+        UpdateMovementState();
+        UpdateAnimatorParameters();
+        UpdateLastTransform();
+    }
 
-        //if (h > 0.01f)
-        //    animatorVeslo.ChangeParentObject(0);
-        //else if (h < -0.01f)
-        //    animatorVeslo.ChangeParentObject(1);
-        //else animatorVeslo.ChangeParentObject(0);
+    private void InitializeRandomCharacter()
+    {
+        if (characterSetups == null || characterSetups.Length == 0)
+        {
+            Debug.LogError("No character setups assigned!");
+            return;
+        }
 
-        lastPosition = GetComponentInParent<Transform>().position;
-        lastRotation = GetComponentInParent<Transform>().rotation;
+        // Деактивируем все модели и весла
+        for (int i = 0; i < characterSetups.Length; i++)
+        {
+            if (characterSetups[i].model != null)
+            {
+                characterSetups[i].model.SetActive(false);
+            }
+            if (characterSetups[i].veslo != null)
+            {
+                characterSetups[i].veslo.SetActive(false);
+            }
+        }
 
+        // Выбираем случайный набор
+        int randomIndex = Random.Range(0, characterSetups.Length);
+        CharacterSetup selectedSetup = characterSetups[randomIndex];
+
+        // Активируем выбранные модель и весло
+        activeModel = selectedSetup.model;
+        activeVeslo = selectedSetup.veslo;
+
+        if (activeModel != null)
+        {
+            activeModel.SetActive(true);
+            animator = activeModel.GetComponent<Animator>();
+            if (animator == null)
+            {
+                Debug.LogWarning("No Animator component found on active model!");
+            }
+        }
+
+        if (activeVeslo != null)
+        {
+            activeVeslo.SetActive(true);
+            vesloController = activeVeslo.GetComponent<ChangeParentVeslo>();
+        }
+    }
+
+    private void CalculateMovementInput()
+    {
+        horizontalInput = Mathf.Sign(lastRotation.y - transform.parent.rotation.y);
+        verticalInput = Mathf.Sign(lastPosition.z - transform.parent.position.z);
+        Debug.Log($"Movement input - H: {horizontalInput}, V: {verticalInput}");
+    }
+
+    private void UpdateMovementState()
+    {
+        isMoving = Mathf.Abs(horizontalInput) > 0.1f || Mathf.Abs(verticalInput) > 0.1f;
+    }
+
+    private void UpdateAnimatorParameters()
+    {
+        if (animator == null)
+        {
+            return;
+        }
+
+        animator.SetFloat("verticalMove", verticalInput);
+        animator.SetFloat("horizontalMove", verticalInput);
+        animator.SetBool("isMove", isMoving);
+    }
+
+    private void UpdateLastTransform()
+    {
+        lastPosition = transform.parent.position;
+        lastRotation = transform.parent.rotation;
+    }
+
+    public void SwitchToRandomCharacter()
+    {
+        if (characterSetups == null || characterSetups.Length < 2)
+        {
+            return;
+        }
+
+        // Получаем все наборы, кроме текущего
+        CharacterSetup[] availableSetups = characterSetups.Where(setup => setup.model != activeModel).ToArray();
+        if (availableSetups.Length == 0)
+        {
+            return;
+        }
+
+        // Деактивируем текущие
+        if (activeModel != null)
+        {
+            activeModel.SetActive(false);
+        }
+        if (activeVeslo != null)
+        {
+            activeVeslo.SetActive(false);
+        }
+
+        // Выбираем новый случайный набор
+        int newIndex = Random.Range(0, availableSetups.Length);
+        CharacterSetup newSetup = availableSetups[newIndex];
+
+        // Активируем новые
+        activeModel = newSetup.model;
+        activeVeslo = newSetup.veslo;
+
+        if (activeModel != null)
+        {
+            activeModel.SetActive(true);
+            animator = activeModel.GetComponent<Animator>();
+        }
+
+        if (activeVeslo != null)
+        {
+            activeVeslo.SetActive(true);
+            vesloController = activeVeslo.GetComponent<ChangeParentVeslo>();
+        }
     }
 }
